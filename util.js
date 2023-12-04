9e7cd0781f9f449b1a51922107bf3ee7c5043705
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-12-05 00:39:56 +0800

    Create util.js

---
+++ util.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { messageLink } = require('discord.js');
+const CharacterAI = require("node_characterai");
+const characterAI = new CharacterAI();
+
+let ch = 'ch id';
+
+const characterAi = async (message) => {
+  if(message.channel.id !== ch) return;
+  message.content = message.content.replace(/^<@!?[0-9]{1,20}> ?/i, '');
+  if(message.content.length < 2 || (isNaN(ch) && ch != message.channel.id)) return;
+  message.channel.sendTyping();
+  try{
+    await characterAI.authenticateWithToken(process.env.caiToken)
+    const chat = await characterAI.createOrContinueChat(process.env.caiId);
+    const response = await chat.sendAndAwaitResponse(message.content, true)
+    console.log(`${message.author.id}: ${message.content}\nKaho: ${response}`)
+    message.reply(response)
+  }catch(e) {
+    console.log(e.stack);
+  }
+}
+
+module.exports = {
+    characterAi
+};
